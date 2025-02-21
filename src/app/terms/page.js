f4360dcbf8a116222e573eac19a3afbd01f8a77e
Picsou <flavien@pictet.net>
2025-02-20 20:15:17 -0800

    push

--- src/app/terms/page.js
+++ src/app/terms/page.js
@@ -872,4 +872,9 @@ export default function Terms() {
       </div>
     </>
   );
-} 
+}
+
+export const metadata = {
+  title: 'Terms of Service | Taller - Maximize your height',
+  description: 'Terms of Service for Taller app - We analyze your growth potential and give you a custom plan to make sure you reach it',
+}; 
