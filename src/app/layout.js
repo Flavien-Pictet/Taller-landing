9c636d12c8455e378a9d7ca3078da13520b5382e
Picsou <flavien@pictet.net>
2025-03-14 13:53:49 -0700

    push

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { rethinkSansMedium, rethinkSansBold } from './fonts'
 import "./globals.css";
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Taller - Height Growth & Prediction App | Official Website',
@@ -114,6 +115,22 @@ export default function RootLayout({ children }) {
                 }
               ]
             })
+          }}
+        />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-MH99J6P2WG"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MH99J6P2WG');
+            `,
           }}
         />
       </head>
