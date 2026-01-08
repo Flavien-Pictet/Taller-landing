23f695ca43fd2b2ec3ff5ffd70be446cc17bd0b2
Picsou <flavien@pictet.net>
2026-01-08 15:24:02 +0100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -49,6 +49,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${rethinkSansMedium.variable} ${rethinkSansBold.variable}`}>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <script 
